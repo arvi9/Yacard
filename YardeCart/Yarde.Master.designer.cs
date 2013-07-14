3561ed9974a57ca91eaa3ee141342825a6d7e602
Rajaraman <rajaramanindia@gmail.com>
2013-07-14 08:54:02 +0530

    Added restclient

--- YardeCart/Yarde.Master.designer.cs
+++ YardeCart/Yarde.Master.designer.cs
@@ -40,6 +40,15 @@ namespace YardeCart {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl welcomeMessage;
         
         /// <summary>
+        /// currentLocation control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl currentLocation;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
